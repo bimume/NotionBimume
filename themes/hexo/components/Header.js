e4dfa9b4f99415e65d6170a2738b475d27f94811
Bimu.me <77833107+bimume@users.noreply.github.com>
2025-01-03 00:54:35 +0900

    Update Header.js

--- themes/hexo/components/Header.js
+++ themes/hexo/components/Header.js
@@ -22,7 +22,7 @@ let windowTop = 0
  * @param {*} param0
  * @returns
  */
-const Header = props => {
+const Header = (props) => {
   const searchDrawer = useRef()
   const { tags, currentTag, categories, currentCategory } = props
   const { locale } = useGlobal()
@@ -56,13 +56,10 @@ const Header = props => {
     throttle(() => {
       const scrollS = window.scrollY
       const nav = document.querySelector('#sticky-nav')
-      // 首页和文章页会有头图
       const header = document.querySelector('#header')
-      // 导航栏和头图是否重叠
-      const scrollInHeader =
-        header && (scrollS < 10 || scrollS < header?.clientHeight - 50) // 透明导航条的条件
 
-      // const textWhite = header && scrollInHeader
+      const scrollInHeader =
+        header && (scrollS < 10 || scrollS < header?.clientHeight - 50)
 
       if (scrollInHeader) {
         nav && nav.classList.replace('bg-white', 'bg-none')
@@ -82,7 +79,6 @@ const Header = props => {
         nav && nav.classList.replace('text-white', 'text-black')
       }
 
-      // 导航栏不在头图里，且页面向下滚动一定程度 隐藏导航栏
       const showNav =
         scrollS <= windowTop ||
         scrollS < 5 ||
@@ -100,17 +96,17 @@ const Header = props => {
   const searchDrawerSlot = (
     <>
       {categories && (
-        <section className='mt-8'>
-          <div className='text-sm flex flex-nowrap justify-between font-light px-2'>
-            <div className='text-gray-600 dark:text-gray-200'>
-              <i className='mr-2 fas fa-th-list' />
+        <section className="mt-8">
+          <div className="text-sm flex flex-nowrap justify-between font-light px-2">
+            <div className="text-gray-600 dark:text-gray-200">
+              <i className="mr-2 fas fa-th-list" />
               {locale.COMMON.CATEGORY}
             </div>
             <Link
               href={'/category'}
               passHref
-              className='mb-3 text-gray-400 hover:text-black dark:text-gray-400 dark:hover:text-white hover:underline cursor-pointer'>
-              {locale.COMMON.MORE} <i className='fas fa-angle-double-right' />
+              className="mb-3 text-gray-400 hover:text-black dark:text-gray-400 dark:hover:text-white hover:underline cursor-pointer">
+              {locale.COMMON.MORE} <i className="fas fa-angle-double-right" />
             </Link>
           </div>
           <CategoryGroup
@@ -121,20 +117,20 @@ const Header = props => {
       )}
 
       {tags && (
-        <section className='mt-4'>
-          <div className='text-sm py-2 px-2 flex flex-nowrap justify-between font-light dark:text-gray-200'>
-            <div className='text-gray-600 dark:text-gray-200'>
-              <i className='mr-2 fas fa-tag' />
+        <section className="mt-4">
+          <div className="text-sm py-2 px-2 flex flex-nowrap justify-between font-light dark:text-gray-200">
+            <div className="text-gray-600 dark:text-gray-200">
+              <i className="mr-2 fas fa-tag" />
               {locale.COMMON.TAGS}
             </div>
             <Link
               href={'/tag'}
               passHref
-              className='text-gray-400 hover:text-black  dark:hover:text-white hover:underline cursor-pointer'>
-              {locale.COMMON.MORE} <i className='fas fa-angle-double-right' />
+              className="text-gray-400 hover:text-black  dark:hover:text-white hover:underline cursor-pointer">
+              {locale.COMMON.MORE} <i className="fas fa-angle-double-right" />
             </Link>
           </div>
-          <div className='p-2'>
+          <div className="p-2">
             <TagGroups tags={tags} currentTag={currentTag} />
           </div>
         </section>
@@ -143,36 +139,27 @@ const Header = props => {
   )
 
   return (
-    <div id='top-nav' className='z-40'>
+    <div id="top-nav" className="z-40">
       <SearchDrawer cRef={searchDrawer} slot={searchDrawerSlot} />
 
       {/* 导航栏 */}
       <div
-        id='sticky-nav'
+        id="sticky-nav"
         style={{ backdropFilter: 'blur(3px)' }}
-        className={
-          'top-0 duration-300 transition-all  shadow-none fixed bg-none dark:bg-hexo-black-gray dark:text-gray-200 text-black w-full z-20 transform border-transparent dark:border-transparent'
-        }>
-        <div className='w-full flex justify-between items-center px-4 py-2'>
-          <div className='flex'>
+        className="top-0 duration-300 transition-all shadow-none fixed bg-none dark:bg-hexo-black-gray dark:text-gray-200 text-black w-full z-20 transform border-transparent dark:border-transparent">
+        <div className="w-full flex justify-center items-center px-4 py-2">
+          {/* 居中 Logo */}
+          <div className="flex items-center">
             <Logo {...props} />
           </div>
 
-          {/* 右侧功能 */}
-          <div className='mr-1 flex justify-end items-center '>
-            <div className='hidden lg:flex'>
-              {' '}
-              <MenuListTop {...props} />
-            </div>
-            <div
-              onClick={toggleMenuOpen}
-              className='w-8 justify-center items-center h-8 cursor-pointer flex lg:hidden'>
-              {isOpen ? (
-                <i className='fas fa-times' />
-              ) : (
-                <i className='fas fa-bars' />
-              )}
-            </div>
+          {/* 居中导航菜单 */}
+          <div className="flex justify-center items-center mx-auto">
+            <MenuListTop {...props} />
+          </div>
+
+          {/* 可选按钮 */}
+          <div className="flex items-center">
             {showSearchButton && <SearchButton />}
             {showRandomButton && <ButtonRandomPost {...props} />}
           </div>
